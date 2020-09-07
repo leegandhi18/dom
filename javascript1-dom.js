0327d70717300fb35be57a7395a59ce6abf0b3ba
leegandhi18 <leegandhi18@gmail.com>
2020-09-08 00:15:20 +0900

    노드 삽입, 노드 선택, 컬럼 정렬

--- javascript1-dom.js
+++ javascript1-dom.js
@@ -69,18 +69,276 @@ window.addEventListener("load",function() {
 
 });
 
-// Ex5 : 엘리먼트 노드의 속성 변경
+// Ex5 : 엘리먼트 노드 & CSS 속성 변경
 window.addEventListener("load",function() {
     var section = document.querySelector("#section5");
     var srcInput = section.querySelector(".src-input");
     var imgSelect = section.querySelector(".img-select");
     var changeButton = section.querySelector(".change-button");
     var img = section.querySelector(".img");
+    var colorInput = section.querySelector(".color-input");
 
     changeButton.onclick = function(){
        img.src = "images/" + srcInput.value;
        // img.src = "images/" + imgSelect.value;
+
+       // img.style.border-color = ?;
+       // img.style["border-color"] = colorInput.value;
+       img.style.borderColor = colorInput.value;
+       console.log(img.className);
     };
     
     
+});
+
+// Ex6 : 노드조작 - 메뉴추가(createTextNode, Elemnent)
+window.addEventListener("load",function() {
+    var section = document.querySelector("#section6");
+    var titleInput = section.querySelector(".title-input");
+    var menuListUl = section.querySelector(".menu-list");
+    var addButton = section.querySelector(".add-button");
+    var delButton = section.querySelector(".del-button");
+
+    addButton.onclick = function(){
+        var title = titleInput.value;
+
+        var html = '<a href="">'+ title + '</a>';
+        var li = document.createElement("li");
+        li.innerHTML = html;
+
+        // menuListUl.appendChild(li);
+        menuListUl.append(li);
+
+        // menuListUl.innerHTML += '<li><a href="">'+ title + '</a></li>';
+
+
+
+        /* var title = titleInput.value;
+        var txtNode = document.createTextNode(title);
+
+        var aNode = document.createElement("a");
+        aNode.href="";
+        aNode.appendChild(txtNode);
+
+        var liNode = document.createElement("li");
+        liNode.appendChild(aNode);
+
+        menuListUl.appendChild(liNode); */
+        
+        // var title = titleInput.value;
+        // var txtNode = document.createTextNode(title);
+        // menuListDiv.appendChild(txtNode);
+    };
+    
+    delButton.onclick = function(){
+        // var txtNode = menuListUl.childNodes[0];
+        
+        var liNode = menuListUl.children[0];
+        // menuListUl.removeChild(liNode);
+        liNode.remove();
+    };
+    
+});
+
+// Ex7 : 노드 복제와 템플릿 태그
+window.addEventListener("load", function(){
+    var notices = [
+        {id:5, title:"퐈이야~~~", regDate:"2019-01-26", writerId:"newlec", hit:0},
+        {id:6, title:"나 좀 복제해줘~", regDate:"2019-01-26", writerId:"newlec", hit:17}
+    ];
+
+    var section = document.querySelector("#section7");
+    
+    var noticeList =section.querySelector(".notice-list"); 
+    var tbodyNode = noticeList.querySelector("tbody");
+    var cloneButton = section.querySelector(".clone-button");
+    var templateButton = section.querySelector(".template-button");
+
+    cloneButton.onclick = function(){
+        var trNode = noticeList.querySelector("tbody tr");
+        var cloneNode = trNode.cloneNode(true);
+        var tds = cloneNode.querySelectorAll("td");
+        tds[0].textContent = notices[0].id;
+        tds[1].innerHTMl = '<a href="' + notices[0].id + '">' + notices[0].title +'</a>';
+        tds[2].textContent = notices[0].regDate;
+        tds[3].textContent = notices[0].writerId;
+        tds[4].textContent = notices[0].hit;
+
+        // tbodyNode.appendChild(cloneNode);
+        tbodyNode.append(cloneNode);
+
+    };
+
+    templateButton.onclick = function(){
+        var template = section.querySelector("template");
+        var cloneNode = document.importNode(template.content, true);
+        var tds = cloneNode.querySelectorAll("td");
+        tds[0].textContent = notices[0].id;
+        
+        // tds[1].innerHTML = '<a href="' + notices[0].id + '">' + notices[0].title +'</a>';
+
+        var aNode = tds[1].children[0];
+        aNode.href = notices[0].id;
+        aNode.textContent = notices[0].title;
+
+        tds[2].textContent = notices[0].regDate;
+        tds[3].textContent = notices[0].writerId;
+        tds[4].textContent = notices[0].hit;
+
+        // tbodyNode.appendChild(cloneNode);
+        tbodyNode.append(cloneNode);
+        
+    };
+
+});
+
+// Ex8 : 노드 삽입과 바꾸기
+window.addEventListener("load", function(){
+
+    var section = document.querySelector("#section8");
+    
+    var noticeList =section.querySelector(".notice-list"); 
+    var tbodyNode = noticeList.querySelector("tbody");
+    var upButton = section.querySelector(".up-button");
+    var downButton = section.querySelector(".down-button");
+
+    var currentNode = tbodyNode.firstElementChild; //.children[0];
+
+    downButton.onclick = function(){
+        var nextNode = currentNode.nextElementSibling;
+        if(nextNode == null) {
+            alert("더 이상 이동할 수 없습니다.");
+            return;
+        }
+
+        // tbodyNode.removeChild(nextNode);
+        // tbodyNode.insertBefore(nextNode, currentNode);
+        currentNode.insertAdjacentElement("beforebegin", nextNode);
+    };
+
+    upButton.onclick = function(){
+        var prevNode = currentNode.previousElementSibling;
+        if(prevNode == null) {
+            alert("더 이상 이동할 수 없습니다.");
+            return;
+        }
+        
+
+        // tbodyNode.removeChild(currentNode);
+        // tbodyNode.insertBefore(currentNode, prevNode);
+        currentNode.insertAdjacentElement("afterend", prevNode);
+    };
+
+});
+
+// Ex9 : 다중 노드선택 방법과 일괄삭제, 노드의 자리바꾸기
+window.addEventListener("load", function(){
+
+    var section = document.querySelector("#section9");
+    
+    var noticeList =section.querySelector(".notice-list"); 
+    var tbody = noticeList.querySelector("tbody");
+    var allCheckbox = section.querySelector(".overall-checkbox");
+    var delButton = section.querySelector(".del-button");
+    var swapButton = section.querySelector(".swap-button");
+
+    allCheckbox.onchange = function(){
+        var inputs = tbody.querySelectorAll("input[type='checkbox']");
+        
+        for(var i = 0; i < inputs.length; i++) {
+            inputs[i].checked = allCheckbox.checked;
+        }
+        
+    };
+
+    delButton.onclick = function(){
+        var inputs = tbody.querySelectorAll("input[type='checkbox']:checked");
+        // console.log(inputs.length);
+        // if(inputs[0].checked)
+
+        for(var i = 0; i < inputs.length; i++)
+            inputs[i].parentElement.parentElement.remove();
+    };
+
+    swapButton.onclick = function(){
+        var inputs = tbody.querySelectorAll("input[type='checkbox']:checked");
+
+        if(inputs.length != 2) {
+            alert("엘리먼트는 2개를 선택해야만 합니다.");
+            return;
+        }
+        
+        var trs = [];
+        for(var i = 0; i < inputs.length; i++) {
+            trs.push(inputs[i].parentElement.parentElement);
+        }
+
+            var cloneNode = trs[0].cloneNode(true);
+            trs[1].replaceWith(cloneNode);
+            trs[0].replaceWith(trs[1])
+    };
+
+});
+
+// Ex10 : 클릭한 컬럼을 기준으로 레코드 정렬하기 #1
+window.addEventListener("load", function(){
+
+    var notices = [
+        {"id":1, "title":"자바스크립트란..", "regDate":"2019-02-02", "writerId":"newlec", "hit":0},
+        {"id":2, "title":"유투브에 끌려다니지 않으려고 했는데....ㅜㅜ..", "regDate":"2019-02-05", "writerId":"newlec", "hit":2},
+        {"id":3, "title":"기본기가 튼튼해야....", "regDate":"2019-02-01", "writerId":"newlec", "hit":1},
+        {"id":4, "title":"근데 조회수가 ㅜㅜ..", "regDate":"2019-01-25", "writerId":"newlec", "hit":0}
+    ];
+
+    var section = document.querySelector("#section10");
+    
+    var noticeList =section.querySelector(".notice-list");
+    var titldTd = section.querySelector(".title");
+    var tbodyNode = noticeList.querySelector("tbody");
+
+    var bindData = function(){
+        var template = section.querySelector("template");
+
+        for(var i=0; i<notices.length; i++){
+            var cloneNode = document.importNode(template.content, true);
+            var tds = cloneNode.querySelectorAll("td");
+            tds[0].textContent = notices[i].id;            
+
+            var aNode = tds[1].children[0];
+            aNode.href=notices[i].id;
+            aNode.textContent = notices[i].title;
+
+            tds[2].textContent = notices[i].regDate;
+            tds[3].textContent = notices[i].writerId;
+            tds[4].textContent = notices[i].hit;
+
+            tbodyNode.appendChild(cloneNode);
+        }
+    };
+
+    bindData();
+
+    var titleSorted = false;
+
+    titldTd.onclick = function(){
+        
+        tbodyNode.innerHTML = "";
+        if(!titleSorted) {
+            notices.sort(function(a, b) {
+                titleSorted = true;
+
+                if(a.title < b.title)
+                    return -1;
+                else if(a.title > b.title)
+                    return 1;
+                else    
+                    return 0;
+            });
+        }
+        else
+            notices.reverse();
+        
+        bindData();
+
+    };
 });
