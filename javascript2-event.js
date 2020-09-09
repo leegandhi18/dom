9f1861184965f4d294878b5c1b03edd8266b2878
leegandhi18 <leegandhi18@gmail.com>
2020-09-10 00:48:15 +0900

    마우스 이벤트

--- javascript2-event.js
+++ javascript2-event.js
@@ -77,4 +77,194 @@ window.addEventListener("load", function(){
         currentImg.insertAdjacentElement("afterend", img);
     };
 
-}); 
+});
+
+// Ex4 : 서로 다른 기능의 여러 버튼을 가진 화면에서 이벤트를 처리하는 방법
+window.addEventListener("load", function(){
+    
+    var section = document.querySelector("#section4");
+    
+    var tbody = section.querySelector(".notice-list tbody");
+
+    tbody.onclick = function(e) {
+        
+        e.preventDefault();
+
+        var target = e.target;
+        
+        if(target.nodeName != "A") {
+            return;
+        }
+
+        // if(target.nodeName != "INPUT") {
+        //     return;
+        // }
+
+        // document.body.classList.contains("sel-button");
+        if(target.classList.contains("sel-button")) {
+            var tr = target.parentElement
+            for(; tr.nodeName != "TR"; tr = tr.parentElement);
+
+            tr.style.background = "yellow";
+        }
+
+        else if(target.classList.contains("edit-button")) {
+
+        }
+
+        else if(target.classList.contains("del-button")) {
+
+        }
+
+    };
+
+});
+
+// Ex5 : Trigger
+window.addEventListener("load", function(){
+    var section = document.querySelector("#section5");
+    var fileButton = section.querySelector(".file-button");
+    var fileTriggerButton = section.querySelector(".file-trigger-button");
+
+    fileTriggerButton.onclick = function(){
+        var event = new MouseEvent("click", {
+            'view': window,
+            'bubbles' : true,
+            'cancelable': true
+        });
+        fileButton.dispatchEvent(event);
+    };
+});
+
+// Ex6 : 마우스 이벤트 객체 - MouseEvent Position
+window.addEventListener("load", function(){
+    var section = document.querySelector("#section6");
+    var container = section.querySelector(".container");
+    var box = section.querySelector(".box");
+
+    container.onclick = function(e){
+        // e.x, e.y / e.offsetX, e.offsetY / e.clientX. e.clientY
+        // e.pageX, e.pageY...
+        console.log("(x,y) : " + e.x +", " + e.y);
+        console.log("client (x,y) : " + e.clientX +", " + e.clientY);
+        console.log("page (x,y) : " + e.pageX +", " + e.pageY);
+        console.log("offset (x,y) : " + e.offsetX +", " + e.offsetY);
+        box.style.position = "absolute";
+        box.style.left = e.pageX+"px";
+        box.style.top = e.pageY+"px";
+
+    };
+});
+
+// Ex7 : 마우스 이벤트 객체 - 드래그 방식으로 박스 옮기기
+window.addEventListener("load", function(){
+    var section = document.querySelector("#section7");
+    var container = section.querySelector(".container");
+    var box = section.querySelector(".box");
+    var dragging = false;
+    var offset = {x:0, y:0};
+    container.onmousedown = function(e){
+        if(e.target === box)
+            dragging = true;
+        
+    };
+
+
+    container.onmousemove = function(e){
+        if(!dragging) return;
+
+        box.style.left = e.pageX - offset.x + "px";
+        box.style.top = e.pageY - offset.y + "px";
+
+    };
+
+    container.onmouseup = function(e){
+        dragging = false;
+    };
+
+    box.onmousedown = function(e) {
+        offset.x = e.offsetX;
+        offset.y = e.offsetY;
+    }
+});
+
+// Ex8 : 마우스 이벤트 객체 - 여러개 박스 드래그 방식으로 옮기기
+window.addEventListener("load", function(){
+    var section = document.querySelector("#section8");
+    var container = section.querySelector(".container");
+    var box = section.querySelector(".box");
+    var dragging = false;
+    var offset = {x:0, y:0};
+    var current = null;
+    container.onmousedown = function(e){
+        if(e.target.classList.contains("box")){
+            dragging = true;
+            current = e.target;
+            offset.x = e.offsetX;
+            offset.y = e.offsetY;
+        }
+        
+    };
+
+
+    container.onmousemove = function(e){
+        if(!dragging) return;
+
+        current.style.left = e.pageX - offset.x + "px";
+        current.style.top = e.pageY - offset.y + "px";
+
+    };
+
+    container.onmouseup = function(e){
+        dragging = false;
+    };
+
+    
+});
+
+// Ex9 : 마우스 이벤트 객체 - 박스의 offset 영역 좌표 이용하기
+window.addEventListener("load", function(){
+    var section = document.querySelector("#section9");
+    var container = section.querySelector(".container");
+    var status = section.querySelector(".status");
+    var dragging = false;
+    var offset = {x:0, y:0};
+    var current = null;
+    var left = container.offsetLeft;
+    var top = container.offsetTop;
+
+    console.log(left);
+    console.log(top);
+
+    // container, document
+    section.onmousedown = function(e){
+        if(e.target.classList.contains("box")){
+            dragging = true;
+            current = e.target;
+            offset.x = e.offsetX;
+            offset.y = e.offsetY;
+        }
+        
+    };
+
+    // container, document
+    section.onmousemove = function(e){
+        if(!dragging) return;
+
+        var x = e.pageX - offset.x - left;
+        var y = e.pageY - offset.y - top;
+
+        current.style.left = x + "px";
+        current.style.top = y + "px";
+
+        status.innerText = "(x, y) : (" + x + ", " + y + ")";
+
+    };
+
+    // container, document
+    section.onmouseup = function(e){
+        dragging = false;
+    };
+
+    
+});
